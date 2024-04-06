4af70fce3b2d0cf1b8c5cf8333b410976b6ef418
Oleksandr Krizhanovsky <krux228@gmail.com>
2024-04-06 17:33:35 +0300

    1

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -4,23 +4,23 @@
   Функція compare повинна повертати AllType.
 */
 
-// type AllType = {
-//   name: string;
-//   position: number;
-//   color: string;
-//   weight: number;
-// };
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
 
-// function compare<T extends AllType, U extends AllType>(
-//   top: Pick<T, "name" | "color">,
-//   bottom: Pick<U, "position" | "weight">
-// ): AllType {
-//   return {
-//     name: top.name,
-//     color: top.color,
-//     position: bottom.position,
-//     weight: bottom.weight,
-//   };
-// }
+function compare<T extends AllType, U extends AllType>(
+  top: Pick<T, "name" | "color">,
+  bottom: Pick<U, "position" | "weight">
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
 
 export {};
